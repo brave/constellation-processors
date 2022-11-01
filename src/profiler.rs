5851e95177550db79653ff5b183002c70fd38984
Darnell Andries <dandries@brave.com>
2022-10-31 19:30:01 -0700

    Use RwLock for profiler

--- src/profiler.rs
+++ src/profiler.rs
@@ -1,6 +1,6 @@
 use derive_more::Display;
-use std::{collections::HashMap, time::Instant};
-use tokio::sync::Mutex;
+use std::{collections::HashMap, ops::DerefMut, time::Instant};
+use tokio::sync::{Mutex, RwLock};
 
 #[derive(Display)]
 enum StatInfo {
@@ -27,7 +27,7 @@ enum StatInfo {
   Seconds(f64),
 }
 
-#[derive(Display, Hash, PartialEq, Eq)]
+#[derive(Copy, Clone, Display, Hash, PartialEq, Eq)]
 pub enum ProfilerStat {
   DownloadTime,
   TaskProcessingTime,
@@ -43,38 +43,45 @@ pub enum ProfilerStat {
 
 #[derive(Default)]
 pub struct Profiler {
-  stats: Mutex<HashMap<ProfilerStat, StatInfo>>,
+  stats: RwLock<HashMap<ProfilerStat, Mutex<StatInfo>>>,
 }
 
 impl Profiler {
   pub async fn record_total_time(&self, key: ProfilerStat, start_instant: Instant) {
     let seconds = start_instant.elapsed().as_secs_f64();
-    let mut stats = self.stats.lock().await;
+    let mut stats = self.stats.write().await;
     assert!(!stats.contains_key(&key), "Total time stat already exists");
-    stats.insert(key, StatInfo::Seconds(seconds));
+    stats.insert(key, Mutex::new(StatInfo::Seconds(seconds)));
   }
 
   pub async fn record_range(&self, key: ProfilerStat, value: f64, unit: &'static str) {
-    let mut stats = self.stats.lock().await;
-    let mut stat_info = stats.entry(key).or_insert(StatInfo::Range {
-      unit,
-      min: f64::MAX,
-      max: 0f64,
-      sum: 0f64,
-      count: 0,
-    });
-    match &mut stat_info {
-      &mut StatInfo::Range {
+    if !self.stats.read().await.contains_key(&key) {
+      let mut stats = self.stats.write().await;
+      stats.insert(
+        key,
+        Mutex::new(StatInfo::Range {
+          unit,
+          min: f64::MAX,
+          max: 0f64,
+          sum: 0f64,
+          count: 0,
+        }),
+      );
+    }
+    let stats = self.stats.read().await;
+    let mut stat_info = stats.get(&key).unwrap().lock().await;
+    match stat_info.deref_mut() {
+      StatInfo::Range {
         min,
         max,
         sum,
         count,
         ..
       } => {
-        if &value < min {
+        if value < *min {
           *min = value;
         }
-        if &value > max {
+        if value > *max {
           *max = value;
         }
         *sum += value;
@@ -92,10 +99,10 @@ impl Profiler {
   }
 
   pub async fn summary(&self) -> String {
-    let stats = self.stats.lock().await;
+    let mut stats = self.stats.write().await;
     let mut lines = stats
-      .iter()
-      .map(|(k, v)| format!("{}: {}", k, v))
+      .drain()
+      .map(|(k, v)| format!("{}: {}", k, v.into_inner()))
       .collect::<Vec<String>>();
     lines.sort();
     lines.join("\n")
