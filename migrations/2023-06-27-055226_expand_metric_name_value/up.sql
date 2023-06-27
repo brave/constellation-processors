c201041f74d7cf25f11a09b8056d0a62a58e8fe4
Darnell Andries <dandries@brave.com>
2023-06-26 22:45:09 -0700

    Expand name column size

---
+++ migrations/2023-06-27-055226_expand_metric_name_value/up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE recovered_msgs ALTER COLUMN metric_name TYPE varchar(64);
+ALTER TABLE recovered_msgs ALTER COLUMN metric_value TYPE varchar(128);
--- src/schema.rs
+++ src/schema.rs
@@ -14,9 +14,9 @@ diesel::table! {
         id -> Int8,
         msg_tag -> Bytea,
         epoch_tag -> Int2,
-        #[max_length = 32]
+        #[max_length = 64]
         metric_name -> Varchar,
-        #[max_length = 32]
+        #[max_length = 128]
         metric_value -> Varchar,
         parent_recovered_msg_tag -> Nullable<Bytea>,
         count -> Int8,
